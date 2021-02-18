e19c2413c80049a387b4d7c0f72e2f04ec39bbe2
hansil18 <hansiljasoliya@gmail.com>
2021-02-18 23:42:28 +0530

    index

--- js/test_set.js
+++ js/test_set.js
@@ -1,6 +1,7 @@
 var entry_state = [];
 var track = 0;
 var lock = 0;
+var entry = 0;
 var state = document.getElementById('lock_state');
 
 function add_exit(id)
@@ -20,25 +21,6 @@ function add_exit(id)
     var z = document.getElementById('exit');
     var s = '<button type="button" class="btn ml-3 my-2 sixth"  id = '+(id)+'>'+'process'+(id)+'</button>';
     z.innerHTML += s;
-    if(entry_state.length != track)
-    {
-        lock = 1;
-        if(lock == 1)
-        {
-            state.innerHTML = '<i class="fa fa-lock pr-2"></i>Lock State';
-        }
-        else 
-        {
-            state.innerHTML = '<i class="fa fa-unlock pr-2" aria-hidden="true"></i>Unlocked state';
-        }
-        var jd = entry_state[track++];
-        console.log(jd);
-        var x = document.getElementById(jd);
-        x.remove();
-        var z = document.getElementById('cs');
-        var s = '<button type="button" class="btn ml-3 my-2 fifth" onclick="add_exit(this.id)" id = '+(jd)+'>'+'process'+(jd)+'</button>';
-        z.innerHTML += s;
-    }
 }
 function add_cs(id)
 {
@@ -48,6 +30,7 @@ function add_cs(id)
     }
     else
     {
+        entry = 0;
         lock = 1;
         if(lock == 1)
         {
@@ -68,26 +51,9 @@ function add_cs(id)
 }
 function add_entry(id)
 {
-    if(lock == 0)
+    if(entry == 0)
     {
-        lock = 1;
-        if(lock == 1)
-        {
-            state.innerHTML = '<i class="fa fa-lock pr-2"></i>Lock State';
-        }
-        else 
-        {
-            state.innerHTML = '<i class="fa fa-unlock pr-2" aria-hidden="true"></i>Unlocked state';
-        }
-        document.getElementById('cs_head').style.display = "";
-        var x = document.getElementById(id);
-        x.remove();
-        var z = document.getElementById('cs');
-        var s = '<button type="button" class="btn ml-3 my-2 fifth" onclick="add_exit(this.id)" id = '+(id)+'>'+'process'+(id)+'</button>';
-        z.innerHTML += s;
-    }
-    else 
-    {
+        entry = 1;
         document.getElementById('entry_head').style.display = "";
         entry_state.push(id);
         var x = document.getElementById(id);
@@ -96,13 +62,20 @@ function add_entry(id)
         var s = '<button type="button" class="btn ml-3 my-2 fourth" onclick="add_cs(this.id)" id = '+(id)+'>'+'process'+(id)+'</button>';
         z.innerHTML += s;
     }
+    else 
+    {
+        entry = 0;
+        alert('deadlock condition start the process again');
+        lockvari();
+    }
 }
 
 function lockvari()
 {
     var n = document.getElementById('process').value;
-    lock = 0;
     //added
+    entry = 0;
+    lock = 0;
     document.getElementById('added_head').innerHTML = "Added State:";
     var t = document.getElementById('added');
     document.getElementById('entry').innerHTML = "";
